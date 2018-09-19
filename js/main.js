b952963ac98a560a2b0be3d90ea0b140cb0ed720
Saifur Rahman <safrrhmn@hotmail.com>
2018-09-18 22:19:56 -0400

    added doc and mapbox key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoic2FpZnVydWRhY2l0eSIsImEiOiJjam04aHc1YWwwMXYyM2tqbzJyb2R4aHUzIn0.kjJH-4pd3FSci8EYlSLFMA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +

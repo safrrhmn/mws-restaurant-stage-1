6fd57a3941c26fa480a2084daface1714924a272
Saifur Rahman <srahman@drfirst.com>
2018-10-05 15:33:07 -0400

    reconfigured sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-var cacheName = 'v1';
-var cacheFiles = [
+/*
+ gratitude to from https://googlechrome.github.io/samples/service-worker/basic/
+*/
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
         './',
         './index.html',
         './css/*',
@@ -8,47 +18,50 @@ var cacheFiles = [
         './js/*',
 ];
 
-self.addEventListener('install', function (e) {
-        var staticCache = new Cache();
-        e.waitUntil(
-                Promise.all([
-                        caches.add(cacheName, staticCache),
-                        staticCache.addAll(cacheFiles)
-                ])
-        );
-})
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
 
-self.addEventListener('activate', function (e) {
-        console.log('[ServiceWorker] Activated');
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
 
-        e.waitUntil(
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-                // Get all the cache keys (cacheName)
-                caches.keys().then(function (cacheNames) {
-                        return Promise.all(cacheNames.map(function (thisCacheName) {
-
-                                // If a cached item is saved under a previous cacheName
-                                if (thisCacheName !== cacheName) {
-
-                                        // Delete that cached file
-                                        console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-                                        return caches.delete(thisCacheName);
-                                }
-                        }));
-                })
-        ); // end e.waitUntil
-
-})
-
-self.addEventListener('fetch', function (e) {
-        console.log('[ServiceWorker] Fetch', e.request.url);
-        e.respondWith(
-                caches.match(e.request).catch(function () {
-                        return event.default();
-                }).catch(function () {
-                        return caches.match('./jezz.html');
-                })
-        );
-
-
-})
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
